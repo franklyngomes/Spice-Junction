07663513957c663bf1a3c6e8e471be0bc79a776c
franklyngomes <franklyngomes15@gmail.com>
2025-09-26 15:14:27 +0530

    Attempt to fix multer-cloudinary error

--- server/src/utils/CloudinaryUpload.ts
+++ server/src/utils/CloudinaryUpload.ts
@@ -1,17 +1,11 @@
 import cloudinary from "../config/cloudinary.js";
-export const uploadToCloudinary = (file: Express.Multer.File) =>
+
+export const uploadToCloudinary = (file: Express.Multer.File, folder = "spice_junction_restaurants") =>
   new Promise<any>((resolve, reject) => {
     const stream = cloudinary.uploader.upload_stream(
-      {
-        folder: "spice_junction_restaurants",
-        resource_type: "auto",
-        public_id: `${Date.now()}-${file.originalname}`,
-      },
+      { folder },
       (error, result) => {
-        if (error) {
-          console.error("Cloudinary upload error:", error);
-          return reject(error);
-        }
+        if (error) return reject(error);
         resolve(result);
       }
     );
