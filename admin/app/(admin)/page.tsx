241921a74d12a5d37c605ab1f7472e5114f17823
franklyngomes <franklyngomes15@gmail.com>
2025-10-03 12:15:10 +0530

    Attempt to fix image URL issue

--- admin/app/(admin)/page.tsx
+++ admin/app/(admin)/page.tsx
@@ -1,6 +1,4 @@
 "use client"
-// import type { Metadata } from "next";
-import { EcommerceMetrics } from "../../components/ecommerce/EcommerceMetrics";
 import React from "react";
 import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
 import StatisticsChart from "../../components/ecommerce/StatisticsChart";
@@ -25,17 +23,13 @@ export default function Ecommerce() {
             {
               restaurantId && role === "restaurant" || role === "admin" ?
                 <div className="grid grid-cols-12 gap-4 md:gap-6">
-                  <div className="col-span-12 space-y-6 xl:col-span-7">
-                    <EcommerceMetrics />
-
+                  <div className="col-span-12">
+                    <StatisticsChart />
                   </div>
                   <div className="col-span-12 xl:col-span-5">
                     <MonthlySalesChart />
                   </div>
 
-                  <div className="col-span-12">
-                    <StatisticsChart />
-                  </div>
 
                   <div className="col-span-12 xl:col-span-5">
                   </div>
