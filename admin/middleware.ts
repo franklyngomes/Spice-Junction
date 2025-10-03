5940897fdc0baee9c2fd2d356cbd3c1c6c7dc894
franklyngomes <franklyngomes15@gmail.com>
2025-10-03 12:08:51 +0530

    Attempt to fix image URL issue

--- admin/middleware.ts
+++ admin/middleware.ts
@@ -15,7 +15,7 @@ export const middleware = (request: NextRequest) => {
 }
 
 export const config = {
-  matcher:  [
-     "/((?!_next/static|_next/image|favicon.ico).*)",
+  matcher: [
+     "/((?!_next|favicon.ico|public|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)"
   ]
 }
