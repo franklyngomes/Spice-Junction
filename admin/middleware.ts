618555ac08667b487c8655e7e6838b4df33a2434
franklyngomes <franklyngomes15@gmail.com>
2025-10-03 11:52:29 +0530

    Attempt to fix deployment issues

--- admin/middleware.ts
+++ admin/middleware.ts
@@ -15,5 +15,7 @@ export const middleware = (request: NextRequest) => {
 }
 
 export const config = {
-  matcher:  ["/((?!_next|favicon.ico|public|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*"]
+  matcher:  [
+     "/((?!_next/static|_next/image|favicon.ico).*)",
+  ]
 }
