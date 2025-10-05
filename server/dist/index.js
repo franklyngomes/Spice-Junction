34d8b6288b6d4fac8dd29692ef36a71b94077c22
franklyngomes <franklyngomes15@gmail.com>
2025-10-05 15:39:48 +0530

    Fixed minor API doc issue

--- server/dist/index.js
+++ server/dist/index.js
@@ -18,9 +18,9 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 const allowedOrigins = [
     "https://spice-junction.onrender.com",
+    "https://spice-junction-admin.onrender.com",
     "http://localhost:5173",
     "http://localhost:3000",
-    "http://localhost:3001",
     "http://localhost:5000",
 ];
 app.set("trust proxy", 1);
