67e2570fe6eaf47f1dd697bd14b307ddf76ad19a
franklyngomes <franklyngomes15@gmail.com>
2025-10-07 01:28:39 +0530

    Access to Admin Signup Page Established

--- admin/layout/AppHeader.tsx
+++ admin/layout/AppHeader.tsx
@@ -245,12 +245,12 @@ const AppHeader: React.FC = () => {
                   </DropdownItem>
                 </li>
                 {
-                  user?.role === "Admin" ?
+                  user?.role === "admin" ?
                     <li>
                       <DropdownItem
                         onItemClick={closeDropdown}
                         tag="a"
-                        href="/signup"
+                        href="/admin-signup"
                         className="flex items-center gap-3 px-3 py-2 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
                       >
                         <UsersIcon />
