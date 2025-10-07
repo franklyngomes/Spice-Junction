47a49d30ee769f5ec9c9f454ed6f0a4246ac04eb
franklyngomes <franklyngomes15@gmail.com>
2025-10-07 13:39:45 +0530

    Attempt to fix the Email Service

--- server/src/helper/EmailService.ts
+++ server/src/helper/EmailService.ts
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY!);
 export async function sendEmail(to: string, subject: string, html: string){
   try {
     const data = await resend.emails.send({
-      from: process.env.EMAIL_SERVICE_USER!,
+      from: "onboarding@resend.dev"  ,
       to,
       subject,
       html
