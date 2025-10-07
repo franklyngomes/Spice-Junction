7341888144bf684faa0d811c182b04182992e314
franklyngomes <franklyngomes15@gmail.com>
2025-10-07 13:28:02 +0530

    Attempt to fix Email Service

--- server/src/helper/EmailService.ts
+++ server/src/helper/EmailService.ts
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY!);
 export async function sendEmail(to: string, subject: string, html: string){
   try {
     const data = await resend.emails.send({
-      from: `Spice Junction <${process.env.EMAIL_SERVICE_USER!}>`,
+      from: process.env.EMAIL_SERVICE_USER!,
       to,
       subject,
       html
