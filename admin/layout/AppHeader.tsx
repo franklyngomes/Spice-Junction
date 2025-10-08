dc46bbb2d163929b11e048189f0be3c29304b444
franklyngomes <franklyngomes15@gmail.com>
2025-10-08 10:12:43 +0530

    Admin Panel Header Update

--- admin/layout/AppHeader.tsx
+++ admin/layout/AppHeader.tsx
@@ -254,7 +254,7 @@ const AppHeader: React.FC = () => {
                         className="flex items-center gap-3 px-3 py-2 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
                       >
                         <UsersIcon />
-                        Create New User
+                        Create New Admin
                       </DropdownItem>
                     </li>
                     : null
