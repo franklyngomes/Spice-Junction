1235a30413fbc030de02f61750bfe014e4d384d7
franklyngomes <franklyngomes15@gmail.com>
2025-12-18 21:11:22 +0530

    Added updated server URL in AxiosInstance & change favicon

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const axiosInstance = axios.create({
-  baseURL: 'https://spice-junction.onrender.com/api', 
+  baseURL: 'https://spice-junction.onrender.com/', 
   headers: { 'Content-Type': 'application/json' }
 })
 
